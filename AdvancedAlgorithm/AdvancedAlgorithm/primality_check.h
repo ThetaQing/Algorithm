e6961cecedccf3da104d0e846d9f0256686109fc
ThetaQing <csusq2016@163.com>
2019-10-30 09:43:51 +0800

    Revert "素数检测"
    This reverts commit 75229f2ab0dc3f44b3b024aec146e12b3b848461.

--- AdvancedAlgorithm/AdvancedAlgorithm/primality_check.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PRIMALITY_CHECK_H__
-#define __PRIMALITY_CHECK_H__
-
-#define COMPOSITE  0// 合数
-#define PRIME  1// 质数
-bool Primality(unsigned __int64 N);
-bool PrimalityImprove(unsigned __int64 N);
-#endif // !__PRIMALITY_CHECK_H__
-#pragma once
