73e8b93b9b67b54a3991b9cdd27a6b683412fa57
ThetaQing <csusq2016@163.com>
2019-11-05 15:17:21 +0800

    选择排序改用普通数组实现，添加基于vector的插入排序
    选择排序：主要是为了说明，在数组、Array、vector中，vector的功能更加强大，提供很多操作函数，但代价是效率较低，对于长度固定的数组，尽量不使用容器。而Array在传递参数时需要传入Array长度，不太适合输入数组长度不定的情况，相比而言，普通数组可以较好平衡，输入参数多一个size变量。
    插入排序：主要是用vector的内置操作函数实现元素的插入，速度较慢，有待改进，下一步将vector换成普通数组实现。

---
+++ AdvancedAlgorithm/AdvancedAlgorithm/sort.h
@@ -0,0 +1,12 @@
+#ifndef __SORT_H__
+#define __SORT_H__
+#include <iostream>
+#include <vector>
+
+void BubbleSort(std::vector <int>& vec);
+void SelectionSort(int* vec, int len);
+void InsertionSort(std::vector<int>& vec);
+#endif
+
+
+#pragma once
